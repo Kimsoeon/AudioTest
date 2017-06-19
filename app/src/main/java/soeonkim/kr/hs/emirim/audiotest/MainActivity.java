6c5ef5a58cb112c5fcfcc1899f0762a07cd0c265
Kimsoeon <a1thdjs@naver.com>
2017-06-19 19:18:16 +0900

    #3 Event Programing

--- app/src/main/java/soeonkim/kr/hs/emirim/audiotest/MainActivity.java
+++ app/src/main/java/soeonkim/kr/hs/emirim/audiotest/MainActivity.java
@@ -1,13 +1,35 @@
 package soeonkim.kr.hs.emirim.audiotest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    Switch switch_music;
+    MediaPlayer musicPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switch_music= (Switch)findViewById(R.id.switch_music);
+        musicPlayer = MediaPlayer.create(this, R.raw.the_physics_of_love);
+        switch_music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switch_music.isChecked())
+                    musicPlayer.start();
+                else
+                    musicPlayer.pause();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStop() {
+        musicPlayer.stop();
     }
 }
